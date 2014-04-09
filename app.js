75d05ac3264251d28cf2b6a29579a1e513d5be18
Neel <neel250294@gmail.com>
2014-04-09 23:37:22 +0530

    email confirmation added git status

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ var http = require('http');
 var path = require('path');
 var contacts=require("./modules/contacts");
 var login=require("./modules/login");
+var money=require("./modules/money");
 var app=express();
 
 //server config
 app.set('port',  process.env.PORT || 3000);
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
-// modules used
+
+//  App modules
 app.use(login);
 app.use(contacts);
+app.use(money);
 app.get('*',function(req,res){res.render('404')});
 
 //server create
--- modules/login/index.js
+++ modules/login/index.js
@@ -1,7 +1,7 @@
-
 /**
- * Module dependencies.
- */
+ * 		Module dependencies.
+ **/
+
 
 var express = require('express');
 var mongoose=require('mongoose');
@@ -10,6 +10,9 @@ var path = require('path');
 var fs = require('fs');
 var bcrypt=require('bcrypt');
 var app = module.exports =express();
+var mailer=require('express-mailer');
+var crypto=require('crypto');
+
 
 // all environments
 app.set('port',  process.env.PORT || 3000);
@@ -26,118 +29,190 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
-app.get('/',function(req,res){
-		if(req.cookies.user_name) res.render('home',{name : req.cookies.user_name})
-		else res.render('index')
-		});
+
+// mailer config 
+mailer.extend(app, {
+  from: 'no-reply@yet-another-app.com',
+  host: 'smtp.gmail.com', // hostname
+  secureConnection: true, // use SSL
+  port: 465, // port for secure SMTP
+  transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+  auth: {
+    user: 'moneylover1438@gmail.com',
+    pass: '2bhuvi8h38'
+  }
+});
 
 // db connection
 var userconn=mongoose.createConnection("mongodb://localhost/users");
+
 
 //db schema
 var userSchema=new mongoose.Schema(
 		{
 name: String,
 email: String,
-passwordhash: String
+passwordhash: String,
+validated : Boolean,
+randomToken :String
 }
 );
+
 //instance variable
 user=userconn.model('user',userSchema);
 
 
 
+// '/' route  
+app.get('/',function(req,res){
+		if(req.cookies.user_name) res.render('home',{name : req.cookies.user_name})
+		else res.render('index')
+		});
 
 
 // add a signup form
 app.get('/signup',function(req,res){
-		if(req.cookies.user_email) res.render("error",{msg : "already logged in! Please logout out first!"})
+		if(req.cookies.user_email) res.render("error",{msg : "Already logged in! Please logout out first!"})
 		else res.render('signup');
 		});
 
+
 //singup form submission
 app.post('/newuser',function(req,res){
-		var b=req.body;
-		bcrypt.genSalt(10, function(err, salt) {
-			bcrypt.hash(b.password, salt, function(err, hash) {
-				if(err) throw err;
-				else 
-				{
+	var b=req.body;
+	var Rtoken;
+	crypto.randomBytes(48,function(ex,buf){
+		Rtoken=buf.toString('hex');
+	});
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(b.password, salt, function(err, hash) {
+			if(err) res.render("error",{msg : "error registering new user"});
+			else 
+			{
 				var userobj={
-
-						name : b.name,
-						email :b.email,
-						passwordhash : hash
-						}
+					name : b.name,
+					email :b.email,
+					passwordhash : hash,
+					randomToken : Rtoken,
+					validated : false
+					}
 				new user(userobj).save(function(err,userobj){
-				if(err) throw err;
-				else
-				{
-					res.cookie('user_email',b.email)
-					res.cookie('user_name',b.name)
-					res.redirect('/home');
-				}
+					if(err) throw err;
+					else{
+						app.mailer.send('invite', {
+						from : "noreply@yap.com",
+  						to: b.email, 
+   						subject: 'Confirm your registration on yet-another-app', 
+    					name: b.name,
+    					token : Rtoken 
+  						},
+  						function (err) {
+    						if (err) {
+           					console.log(err);
+    						res.render('error' ,{msg:'There was an error sending the email'});
+    						}
+    					res.redirect('/');
+  						})
+					}
 				});
-
-				}
-			});
+			}
 		});
+	});
+});
 
+//token searching
+app.param('token',function(req,res,next,token){
+	user.find({randomToken : token},function(err,docs){
+		if(!docs[0]) res.render("error",{msg :"Invalid token to confirm registration"})
+		else
+		{
+			req.user=docs[0];
+			next();
+		}
+	});
+});
+
+// confirming User Email id
+app.get('/invite/:token',function(req,res){
+	user.update({randomToken :req.user.randomToken},{validated : true},function(err){
+		if(err) res.render("error",{msg:"Could not validate the user try again"});
+		else
+		{	
+			res.cookie('user_email',req.user.email)
+			res.cookie('user_name',req.user.name)
+			res.redirect('/home');
+		}
+	});
 });
 
 //login page
 app.get('/login',function(req,res){
-		if(req.cookies.user_email) res.send("error",{msg :"Already logged in"});
-		else res.render('login');
-		});
+	if(req.cookies.user_email) res.send("error",{msg :"Already logged in!! Please logout first"});
+	else res.render('login');
+});
 
 
 // login submission
 app.post('/loginuser',function(req,res){
-		var b=req.body;
-		user.find({email : b.email},function(err,docs){
-			if(!docs[0]) res.render('error',{msg : "Invalid Username"});
-			else{
+	var b=req.body;
+	user.find({email : b.email},function(err,docs){
+		if(!docs[0]) res.render('error',{msg : "Invalid Username"});
+		else{
 			var lol=docs[0];
-			bcrypt.compare(b.password,lol.passwordhash, function(err,isloggedin) {	
-			if(isloggedin)
+			if(lol.validated)
 			{
-			res.cookie('user_email',b.email)
-			res.cookie('user_name',lol.name)
-			res.redirect('/home')
+				bcrypt.compare(b.password,lol.passwordhash, function(err,isloggedin) {	
+				if(isloggedin)
+				{
+					res.cookie('user_email',b.email)
+					res.cookie('user_name',lol.name)
+					res.redirect('/home')
+				}
+				else res.render('error',{msg: "Wrong Password"})
+				});
 			}
-			else res.render('error',{msg: "Wrong Password"})
-			});
+			else
+			{
+				res.render("validate",{msg:"Please validate your Email Address"})
 			}
-			});
-		});
+		}
+	});
+});	
+
 
 
 //Checking Authentication
 function checkauth(req, res, next) {
-	if (!req.cookies.user_email) {
+	if (!req.cookies.user_email) 
+	{
 		res.redirect('/login')
-	} else {
+	} 
+	else
+	{
 		res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
 		next()
 	}
 }
 
+
 // Home page
-app.get('/home',checkauth,function(req,res)
-		{
-		res.render('home',{name : req.cookies.user_name});
-		});
+app.get('/home',checkauth,function(req,res){
+	res.render('home',{name : req.cookies.user_name});
+});
+
+
 //Settings Page
 app.get("/settings",checkauth,function(req,res){
-		user.find({email : req.cookies.user_email},function(err,docs){
-						res.render("settings",{name : req.cookies.user_name,c :docs[0]})
-		});
+	user.find({email : req.cookies.user_email},function(err,docs){
+		res.render("settings",{name : req.cookies.user_name,c :docs[0]})
 	});
+});
+
 
 //change password 
 app.post("/changepasswd",checkauth,function(req,res){
@@ -160,10 +235,6 @@ app.post("/changepasswd",checkauth,function(req,res){
 	});
 });
 		
-			
-
-
-
 
 //Logout function
 app.get('/logout',checkauth,function(req,res){
